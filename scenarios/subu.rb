6a8993f302e8784d34fb19b01701a77f717be5e7
Søren Skovsbøll <soren.skovsboll@tmrw.com>
2023-12-06 15:26:17 +0100

    fix spec scenarios

--- scenarios/subu.rb
+++ scenarios/subu.rb
@@ -1,18 +1,18 @@
 # frozen_string_literal: true
 
-file 'test-result.md', ins: ['./spec/*.rb', './src/*.rb'] do |ins, outs|
-  rtx "bundle exec rspec #{ins.groups[0].join ' '} --out #{outs[0]}"
+file 'test-result.md', ins: ['./spec/*.rb', './src/*.rb'] do |_ins, outs|
+  rtx "bundle exec rspec --out #{outs[0]}"
 end
 
 file('test-result2.md', ins: [
        './spec/*.rb',
        './src/*.rb'
-     ]) do |ins, outs|
-  rtx "bundle exec rspec #{ins.groups[0].join ' '} --out #{outs[0]}"
+     ]) do |_ins, outs|
+  rtx "bundle exec rspec --out #{outs[0]}"
 end
 
-file('test-result3.md', ins: './spec/*.rb') do |ins, outs|
-  rtx "bundle exec rspec #{ins.groups[0].join ' '} --out #{outs[0]}"
+file('test-result3.md', ins: './spec/*.rb') do |_ins, outs|
+  rtx "bundle exec rspec --out #{outs[0]}"
 end
 
 file 'app.zip', ins: 'src/generated_code/api.rb' do |ins, outs|
---
+++ scenarios/test-result.md
@@ -0,0 +1,5 @@
+.
+
+Finished in 0.00076 seconds (files took 0.04916 seconds to load)
+1 example, 0 failures
+
