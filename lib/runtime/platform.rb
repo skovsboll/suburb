7fb49c126acc47bd3bc5c16d786b8a0c37d72936
Søren Skovsbøll <soren.skovsboll@tmrw.com>
2023-12-14 09:54:28 +0100

    add platform

---
+++ lib/runtime/platform.rb
@@ -0,0 +1,24 @@
+require 'rbconfig'
+
+module Suburb
+  module Runtime
+    module Platform
+      def os
+        case (host_os = RbConfig::CONFIG['host_os'])
+        when /mswin|windows/i
+          :windows
+        when /linux|unix/i
+          :linux
+        when /darwin|mac os/i
+          :macos
+        else
+          host_os
+        end
+      end
+
+      def cpu
+        RbConfig::CONFIG['host_cpu'].to_sym
+      end
+    end
+  end
+end
