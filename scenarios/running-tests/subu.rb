27f5cad1418ee9a1fb8958f3d477de49f496aea4
Søren Skovsbøll <sorens@hey.com>
2023-12-15 19:04:58 +0100

    loose last traces of stdout argument

--- scenarios/running-tests/subu.rb
+++ scenarios/running-tests/subu.rb
@@ -1,13 +1,16 @@
 # frozen_string_literal: true
 
 file 'test-result.md', ins: ['./spec/*.rb', './src/*.rb'] do |_ins, outs|
-  rtx "bundle exec rspec --out #{outs[0]}", stdout: true
+  rtx "bundle exec rspec --out #{outs[0]}"
+  sh "cat #{outs[0]}"
 end
 
 file('test-result2.md', ins: ['./spec/*.rb', '../libary/src/*.rb']) do |_ins, outs|
-  rtx "bundle exec rspec --out #{outs[0]}; cat #{outs[0]}", stdout: true
+  rtx "bundle exec rspec --out #{outs[0]}"
+  sh "cat #{outs[0]}"
 end
 
 file('test-result3.md', ins: './spec/*.rb') do |_ins, outs|
-  rtx "bundle exec rspec --out #{outs[0]}; cat #{outs[0]}", stdout: true
+  rtx "bundle exec rspec --out #{outs[0]}"
+  sh "cat #{outs[0]}"
 end
