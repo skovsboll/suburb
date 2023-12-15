74c5053a7e30a24bb4de91b9cc2ccd53108f1289
Søren Skovsbøll <soren.skovsboll@tmrw.com>
2023-12-15 15:36:03 +0100

    add shell platform detection

--- lib/runtime/platform.rb
+++ lib/runtime/platform.rb
@@ -19,6 +19,19 @@ module Suburb
       def cpu
         RbConfig::CONFIG['host_cpu'].to_sym
       end
+
+      def shell
+        shell_path = ENV['SHELL'] || ENV['COMSPEC']
+
+        return :bash if shell_path&.include?('bash')
+        return :zsh if shell_path&.include?('zsh')
+        return :pwsh if shell_path&.include?('pwsh')
+        return :sh if shell_path&.include?('/sh') # Generic sh
+        return :cmd if shell_path&.include?('cmd.exe') # Windows Command Prompt
+        return :powershell if shell_path&.include?('powershell')
+
+        :unknown
+      end
     end
   end
 end
