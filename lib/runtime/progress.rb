ecb8bd249b88807ca647a30360198117a0851d06
Søren Skovsbøll <soren.skovsboll@tmrw.com>
2023-12-19 10:21:59 +0100

    progressbar shows realtive paths

--- lib/runtime/progress.rb
+++ lib/runtime/progress.rb
@@ -10,12 +10,16 @@ module Suburb
         yellow = pastel.yellow('⬚')
 
         status = clean ? 'cleaning' : 'building'
+        main_path = nodes.last.path.relative_path_from(Pathname.pwd)
         bar = TTY::ProgressBar::Multi.new("#{nodes.last.original_path} #{pastel.dim(status)} :bar",
                                           total: nodes.size,
                                           bar_format: :crate,
                                           complete: green,
                                           incomplete: yellow)
-        nodes_with_bars = nodes.map { [_1, bar.register("#{_1.original_path}", total: 1)] }
+        nodes_with_bars = nodes.map do
+          path = _1.path.relative_path_from(Pathname.pwd)
+          [_1, bar.register("#{path}", total: 1)]
+        end
         nodes_with_bars.each do |node, sub_bar|
           block[node]
           sub_bar.advance
